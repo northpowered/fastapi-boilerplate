2cb0b0e5e5874064e4bc342ad57878ed287e8714
romanov <romanov@mail.mm71>
2022-07-28 13:49:00 +0300

    CLI: creating user

--- cli/accounting.py
+++ cli/accounting.py
@@ -1,41 +1,50 @@
 import typer
 import asyncio
+from enum import Enum
 from fastapi.exceptions import HTTPException
 
-
-async def create_superuser(username: str, password: str, email: str):
+async def create_user(username: str, password: str, email: str, superuser: bool=False):
     from accounting.users.models import User
-    return await User.add(username,password,email, as_superuser=True)
-
-
+    return await User.add(username,password,email, as_superuser=superuser)
 
 app = typer.Typer(
     no_args_is_help=True,
     short_help='Operations with users and other AAA objects'
 )
 
+class CreatingObjects(str, Enum):
+    superuser: str = "superuser"
+    user: str = "user"
+    role: str = "role"
 
-@app.command(short_help='Creating objects')
-def create(object: str):
+@app.command(short_help='Creating objects', no_args_is_help=True)
+def create(object: CreatingObjects):
     """
-    Objects:
-
-        - superuser
+    Creating AAA objects
     """
-    from accounting.users.models import User
     match object:
         case 'superuser':
             username = typer.prompt("Username")
             password = typer.prompt("Password")
             email = typer.prompt("Email")
             try:
-                resp: User = asyncio.run(create_superuser(username,password,email))
+                resp = asyncio.run(create_user(username,password,email,True))
             except HTTPException as ex:
                 print('Operation failed')
             else:
                 print(f'Superuser {username} was created with id {resp.id}')
+        case 'user':
+            username = typer.prompt("Username")
+            password = typer.prompt("Password")
+            email = typer.prompt("Email")
+            try:
+                resp = asyncio.run(create_user(username,password,email))
+            except HTTPException as ex:
+                print('Operation failed')
+            else:
+                print(f'User {username} was created with id {resp.id}')
         case _:
-            print(f"init!")
+            print(f"init! {object}")
 
 if __name__ == "__main__":
     app()
