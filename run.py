58ef9c4e2a3b040973c44ef740e80ff78f12a54b
romanov <romanov@mail.mm71>
2022-08-01 15:10:22 +0300

    app prototype

--- run.py
+++ run.py
@@ -4,11 +4,15 @@ import os
 def run_app(config_file: str, reload: bool):
     import uvicorn #mypy
     import os
+    from app import create_app
     os.environ['X_FA_CONF_FILE'] = config_file
     from configuration import config
-
+    from utils.logger import setup_logging
+    setup_logging()
+    from loguru import logger
+    app = create_app()
     uvicorn.run(
-        "app:app",
+        app,
         reload=reload, 
         host=config.server.bind_address,
         port=config.server.bind_port,
